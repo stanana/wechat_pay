ae5e17bec9144a73c6dee9a142ed2f264c31a747
xmcc <2732434911@qq.com>
2019-06-21 11:31:49 +0800

    wechat_pay

---
+++ src/main/java/com/xmcc/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.xmcc.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class TestController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello springboot";
+    }
+}
